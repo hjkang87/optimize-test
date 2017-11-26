e8558dc5c8bceac592260180da9c5422a930f975
Hyunjung Kang <kanghyunjung@gmail.com>
2017-11-26 22:32:27 +0900

    add mvt, redirect test pages
    Signed-off-by: Hyunjung Kang <kanghyunjung@gmail.com>

--- app.py
+++ app.py
@@ -11,5 +11,20 @@ def index():
     return render_template('index.html')
 
 
+@app.route("/mvt", methods=['GET'])
+def multivariate_test():
+    return render_template('mvt.html')
+
+
+@app.route("/redirect_test", methods=['GET'])
+def redirect_test():
+    return render_template('redirect_test.html')
+
+
+@app.route("/redirect_test_new_page", methods=['GET'])
+def redirect_test_new_page():
+    return render_template('redirect_test_new_page.html')
+
+
 if __name__ == "__main__":
     app.run(threaded=True,  debug=True, host='0.0.0.0')
